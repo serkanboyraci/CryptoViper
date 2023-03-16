1d1db562ab509d934ea3df24b6d79bf8545174f7
serkanboyraci <serkanboyraci@gmail.com>
2023-03-16 21:20:34 +0300

    view updated

--- CryptoViper/View.swift
+++ CryptoViper/View.swift
@@ -14,9 +14,76 @@ import UIKit
 
 protocol AnyView {
     
+    var presenter : AnyPresenter? {get set}
+    
+    func update(with cryptos: [Crypto])
+    func update(with error: String)
+    
 }
 
-class CryptoViewController : UIViewController, AnyView {
+class CryptoViewController : UIViewController, AnyView, UITableViewDelegate, UITableViewDataSource {
+    var presenter: AnyPresenter?
+    
+    var cryptos : [Crypto] = []
+    
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true // after taking data we will show tableview
+        return table
+    }()
+    
+    private let messageLabel : UILabel = {
+        let label = UILabel()
+        label.isHidden = false
+        label.text = "Downloading..."
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    override func viewDidLayoutSubviews() { //because of we didnt use storyboard, eachtime we update subviews.
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds // to use same size of screen
+        messageLabel.frame = CGRect(x: view.frame.width/2 - 100, y: view.frame.height/2 - 50,
+                                    width: 200, height: 50)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cryptos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+
+    
+    func update(with cryptos: [Crypto]) {
+        DispatchQueue.main.async {
+            self.cryptos = cryptos
+            self.messageLabel.isHidden = true
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+    }
+    
+    func update(with error: String) {
+        //
+    }
+    
     
 }
 
